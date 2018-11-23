d65bd8f754dfdf2b94a432da210a28b71d985617
David Miller <davidfmiller@me.com>
2018-11-23 11:12:40 -0800

    scroll

--- src/js/billboard.js
+++ src/js/billboard.js
@@ -14,22 +14,28 @@
    */
   Billboard = function(config) {
 
-    const node = RMR.Node.get(config.node);
+    const node = RMR.Node.get('.rmr-billboard');
     if (! node) {
-      throw new Error('Invalid billboard <' + JSON.stringify(config.node) + '>');
+      throw new Error('No billboard <.rmr-billboard> found');
     }
 
     const
+    self = this,
     scroller = RMR.Node.create('div', { class: 'rmr-billboard-scroll' }),
     theme = config.hasOwnProperty('theme') ? config.theme : null,
     height = window.innerHeight;
 
     scroller.addEventListener('click', function() {
-      RMR.Browser.scrollTo(height, 200);
+      self.scroll();
     });
 
     node.appendChild(scroller);
     node.style.minHeight = height + 'px';
+  };
+
+  Billboard.prototype.scroll = function() {
+    const height = parseInt(window.getComputedStyle(RMR.Node.get('.rmr-billboard')).height, 10);
+    RMR.Browser.scrollTo(height, 200);
   };
 
   module.exports = Billboard;
